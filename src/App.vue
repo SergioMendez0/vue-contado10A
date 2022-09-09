31f1a8066c9cc9c288ef1cc6ad72e4ce7c25aa30
SergioMendez0 <a3519110530@alumno.uttehuacan.edu.mx>
2022-09-09 15:43:39 -0500

    VUE3js

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="alert alert-info" role="alert">
+    Sergio Martinez Mendez 10° "A"
+  </div>
+  <br>
+  <img src="./assets/logo.svg" alt="Logo VUE" with="125" height="125">
+  <h1>Hola desde VUE.js 3</h1>
+  <hr>
+  <h2>Contador 0</h2>
+  <button>+1</button>
+  <button>-1</button>
+  <button>Reset</button>
+</template> 
+
+<style scoped>
+  h1 {
+    color: red;
+  }
+</style>
+
+
